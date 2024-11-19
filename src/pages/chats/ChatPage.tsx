8f4017c2431189f301aa8ec10df6d79782b99b10
b9benign <100318396+b9benign@users.noreply.github.com>
2024-11-19 16:31:00 +0100

    chat title is now being fetched and displayed

--- src/pages/chats/ChatPage.tsx
+++ src/pages/chats/ChatPage.tsx
@@ -8,8 +8,6 @@ export default function ChatPage(): React.JSX.Element {
     const { chatId } = useParams();
     if (!chatId) throw new Error("51f90f3c-c782-4914-9726-cc0b855f9de4");
 
-    //TODO: fetch chat.name to display as document title
-
     return (
         <Page documentTitle={"Chat"}>
             <Chat chatId={chatId} />
